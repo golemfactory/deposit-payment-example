f72edd6e70f9bd477e49f9a6a7a3563c174ead74
pociej <grzegorz.pociejewski@gmail.com>
2024-02-26 07:57:21 +0100

    chore: scaffold dev env
    - configure husky + commit linting
    - add git ignore
    - confgure ts
    - setup pnpm
    
    related to #1

---
+++ check-for-issue-reference.js
@@ -0,0 +1,8 @@
+const commitMsg = process.argv[2];
+
+if (!/#[0-9]+/.test(commitMsg)) {
+  console.error(
+    "Error: Commit message must include a reference to an issue (e.g., #123)."
+  );
+  process.exit(1);
+}
